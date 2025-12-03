ac1372ba07429faaebe2ba272a56e0a20044ff9b
nullclyze <229468531+nullclyze@users.noreply.github.com>
2025-12-03 19:26:37 +0400

    Delete main/core/out directory

--- main/core/out/api/transfer.js
+++
@@ -1,8 +0,0 @@
-export function transmit(res, type, data) {
-    try {
-        res.json({ type: type, data: data });
-    }
-    catch (error) {
-        console.log(`Ошибка передачи данных: ${error}`);
-    }
-}
--- main/core/out/preload.js
+++
@@ -1,6 +0,0 @@
-import { contextBridge, ipcRenderer } from 'electron';
-contextBridge.exposeInMainWorld('client', {
-    port: () => ipcRenderer.invoke('client', 'port').then(answer => answer.result),
-    info: () => ipcRenderer.invoke('client', 'get-info').then(answer => answer.result),
-    window: (action) => ipcRenderer.invoke('client', 'window', { action: action }).then(answer => answer.result)
-});
