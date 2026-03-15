c8e9ef6abe30fcb23ec380326c186f3f506f2d43
nullclyze <fliget45d1mw@gmail.com>
2026-03-15 10:57:54 +0400

    Added the ability to import or export accounts

--- src/helpers/date.ts
+++ src/helpers/date.ts
@@ -1,12 +1,18 @@
-export function date(format: string = 'H:M:S'): string {
+export function date(format: string = 'common'): string {
   const date = new Date();
 
   const hours = date.getHours().toString().padStart(2, '0');
   const minutes = date.getMinutes().toString().padStart(2, '0');
   const seconds = date.getSeconds().toString().padStart(2, '0');
 
-  if (format === 'H:M:S') {
+  if (format === 'common') {
     return `${hours}:${minutes}:${seconds}`;
+  } else if (format === 'exact') {
+    const day = date.getDate().toString().padStart(2, '0');
+    const month = (date.getMonth() + 1).toString().padStart(2, '0');
+    const year = date.getFullYear().toString();
+
+    return `${day}.${month}.${year}`;
   } else {
     return `${hours}:${minutes}`;
   }
