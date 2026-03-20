d6b3e50f5fc1f53a27f0c5f15bedc8a13e21f85d
nullclyze <nullclyze@gmail.com>
2026-03-20 21:48:37 +0400

    Edits, improvements

--- src/main.ts
+++ src/main.ts
@@ -20,6 +20,7 @@ import { download } from './utils/downloader';
 import { disableParticles, enableParticles } from './particles';
 import { switchControlWrapper, switchGlobalWrapper } from './utils/switchers';
 import { triggerRegistry } from './modules/trigger_registry';
+import { pluginManager } from './modules/plugin_manager';
 
 
 Chart.register(...registerables);
@@ -127,175 +128,6 @@ async function initUserGuide(): Promise<void> {
     }
   } catch (error) {
     logger.log(`Ошибка загрузки руководства: ${error}`, 'error');
-  }
-}
-
-/** Функция обновления и сохранения состояния плагина (включен / выключен). */
-export function updatePluginState(name: string, state: boolean) {
-  if (process === 'sleep') {
-    plugins[name].enable = state;
-
-    localStorage.setItem(`plugin-state:${name}`, String(state));
-
-    const status = document.getElementById(`${name}-status`) as HTMLElement;
-    const toggler = document.getElementById(`${name}-toggler`) as HTMLButtonElement;
-
-    if (state) {
-      status.innerText = 'Включен';
-
-      toggler.setAttribute('state', 'false');
-      toggler.style.color = '#4ed618';
-      toggler.innerText = 'Включен';
-    } else {
-      status.innerText = 'Выключен';
-
-      toggler.setAttribute('state', 'true');
-      toggler.style.color = '#e61919';
-      toggler.innerText = 'Выключен';
-    }
-  } else {
-    logger.log(`Не удалось изменить состояние плагина ${name}: Plugin states can only be changed before startup`, 'warning');
-  }
-}
-
-/** Функция инициализации карточек плагинов. */
-function initPlugins(): void {
-  try {
-    for (const name in plugins) {
-      const plugin = plugins[name];
-
-      const pluginCard = document.createElement('div');
-      pluginCard.className = 'plugin-card';
-      pluginCard.id = `${name}-plugin`;
-
-      pluginCard.innerHTML = `
-        <div class="head">
-          <svg class="image" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
-            <path fill-rule="evenodd" d="M1 8a7 7 0 1 1 2.898 5.673c-.167-.121-.216-.406-.002-.62l1.8-1.8a3.5 3.5 0 0 0 4.572-.328l1.414-1.415a.5.5 0 0 0 0-.707l-.707-.707 1.559-1.563a.5.5 0 1 0-.708-.706l-1.559 1.562-1.414-1.414 1.56-1.562a.5.5 0 1 0-.707-.706l-1.56 1.56-.707-.706a.5.5 0 0 0-.707 0L5.318 5.975a3.5 3.5 0 0 0-.328 4.571l-1.8 1.8c-.58.58-.62 1.6.121 2.137A8 8 0 1 0 0 8a.5.5 0 0 0 1 0"/>
-          </svg>
-
-          <div class="text">
-            <div class="header" id="${name}-header">${plugin.header}</div>
-            <div class="meta">
-              <span translator-tag="label:plugin-status">Статус:</span> <span id="${name}-status">Выключен</span>
-            </div>
-          </div>
-        </div>
-
-        <div class="controls">
-          <button class="button" plugin-toggler="true" plugin-name="${name}" state="true" id="${name}-toggler" style="color: #d61818;">Выключен</button>
-          <button class="button" plugin-open-description="true" path="${name}-plugin-description">Описание</button>
-        </div>
-      `;
-
-      document.getElementById('plugin-list')?.appendChild(pluginCard);
-      
-      const element = document.createElement('div');
-
-      element.className = 'cover';
-      element.id = `${name}-plugin-description`;
-
-      element.innerHTML = `
-        <div class="panel with-header">
-          <div class="left">
-            <div class="header">${plugin.header} - Описание</div>
-          </div>
-
-          <div class="right">
-            <button class="btn min pretty" plugin-close-description="true" path="${name}-plugin-description">
-              ⨉
-            </button>
-          </div>
-        </div>
-
-        <div class="plugin-description-body">
-          <div class="description" id="${name}-description"></div>
-          <p class="plugin-latest-update">Последнее обновление: <span id="${name}-latest-update">?</span></p>
-        </div>
-      `;
-
-      document.getElementById('plugins-container')?.appendChild(element);
-    }
-
-    document.querySelectorAll('[plugin-toggler="true"]').forEach(t => t.addEventListener('click', () => {
-      const name = t.getAttribute('plugin-name') || '';
-      const state = t.getAttribute('state') === 'true';
-      updatePluginState(name, state);
-    }));  
-
-    document.querySelectorAll('[plugin-open-description="true"]').forEach(e => e.addEventListener('click', () => {
-      const path = e.getAttribute('path');
-      if (!path) return;
-      (document.getElementById(path) as HTMLElement).style.display = 'flex';
-    })); 
-
-    document.querySelectorAll('[plugin-close-description="true"]').forEach(e => e.addEventListener('click', () => {
-      const path = e.getAttribute('path');
-      if (!path) return;
-      (document.getElementById(path) as HTMLElement).style.display = 'none';
-    })); 
-  } catch (error) {
-    logger.log(`Ошибка инициализации плагинов: ${error}`, 'error');
-  }
-}
-
-/** Функция инициализации описаний плагинов. */
-async function initPluginDescriptions(): Promise<void> {
-  try {
-    const content = await download('https://raw.githubusercontent.com/nullclyze/SalarixiOnion/refs/heads/main/salarixi.plugins.json');
-    
-    if (!content) return;
-
-    for (const plugin of content['list']) {
-      const name = plugin['name'];
-      const description = plugin['description'];
-      const latestUpdate = plugin['latest-update'];
-
-      if (plugins[name]?.date) {
-        if (latestUpdate != plugins[name].date) {
-          document.getElementById(`${name}-plugin`)?.classList.add('deprecated');
-          
-          const tag = document.createElement('span');
-          tag.className = 'tag';
-          tag.innerText = 'Устарел';
-
-          document.getElementById(`${name}-header`)?.appendChild(tag);
-        }
-
-        const pluginDescriptionContainer = document.getElementById(`${name}-description`) as HTMLElement;
-        const pluginLatestUpdateContainer = document.getElementById(`${name}-latest-update`) as HTMLElement;
-
-        for (const p of description) {
-          const el = document.createElement('p');
-          el.innerText = p;
-
-          pluginDescriptionContainer.appendChild(el);
-        }
-
-        pluginLatestUpdateContainer.innerText = latestUpdate;
-      } else {
-        const pluginCard = document.createElement('div');
-        pluginCard.className = 'plugin-card';
-        pluginCard.classList.add('unavailable');
-
-        pluginCard.innerHTML = `
-          <div class="head">
-            <svg class="image" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
-              <path fill-rule="evenodd" d="M1 8a7 7 0 1 1 2.898 5.673c-.167-.121-.216-.406-.002-.62l1.8-1.8a3.5 3.5 0 0 0 4.572-.328l1.414-1.415a.5.5 0 0 0 0-.707l-.707-.707 1.559-1.563a.5.5 0 1 0-.708-.706l-1.559 1.562-1.414-1.414 1.56-1.562a.5.5 0 1 0-.707-.706l-1.56 1.56-.707-.706a.5.5 0 0 0-.707 0L5.318 5.975a3.5 3.5 0 0 0-.328 4.571l-1.8 1.8c-.58.58-.62 1.6.121 2.137A8 8 0 1 0 0 8a.5.5 0 0 0 1 0"/>
-            </svg>
-
-            <div class="text">
-              <div class="header">${plugin['header']} <span class="tag">Недоступен</span></div>
-              <div class="meta">Статус: <span class="status">Недоступен</span></div>
-            </div>
-          </div>
-        `;
-
-        document.getElementById('plugin-list')?.appendChild(pluginCard);
-      }
-    }
-  } catch (error) {
-    logger.log(`Ошибка загрузки информации о плагинах: ${error}`, 'error');
   }
 }
 
@@ -438,8 +270,8 @@ async function controlBots(name: string, state: boolean | string): Promise<void>
   }
 }
 
-/** Функция инициализации различных функций. */
-async function initFunctions(): Promise<void> {
+/** Функция инициализации глобальных элементов. */
+function initGlobalElements(): void {
   const startBotsProcessBtn = document.getElementById('start') as HTMLButtonElement;
   const stopBotsProcessBtn = document.getElementById('stop') as HTMLButtonElement;
   const setRandomValuesBtn =  document.getElementById('random') as HTMLButtonElement;
@@ -579,14 +411,7 @@ async function initFunctions(): Promise<void> {
     (document.getElementById('settings_option_bots-count') as HTMLInputElement).value = '';
     (document.getElementById('settings_option_join-delay') as HTMLInputElement).value = '';
   });
-
-  document.getElementById('interface_select_discord-rpc')?.addEventListener('change', async () => await invoke('set_discord_rpc', { state: (document.getElementById('interface_select_discord-rpc') as HTMLSelectElement).value === 'enable' }));
-  await invoke('set_discord_rpc', { state: (document.getElementById('interface_select_discord-rpc') as HTMLSelectElement).value === 'enable' });
-
-  await initUserGuide();
-  await initPluginDescriptions();
-  await initDownloadCount();
-} 
+}
 
 /** Обёрточная функция для пополнения реестра триггеров. */
 export function replenishTriggerRegistry(): void {
@@ -1187,34 +1012,32 @@ async function listenEvents(): Promise<void> {
   });
 }
 
-// Вызывающийся слушатель при загрузке DOMContent.
-document.addEventListener('DOMContentLoaded', async () => {
+// Слушатель события DOMContent у document (#1, синхронный).
+document.addEventListener('DOMContentLoaded', () => {
+  // Инициализируем логгер и сообщения.
   logger.init();
-  logger.log('Клиент запущен', 'info');
-
   messages.init();
+
+  logger.log('Клиент запущен', 'info');
 
   try {
     logger.log('Инициализация, пожайлуста, подождите...', 'extended');
 
+    // Изначально инициализируем глобальные элементы, пополняем реестр триггер-функций.
     initTitleBar();
-    
+    initGlobalElements();
     replenishTriggerRegistry();
 
     document.querySelectorAll('[global="true"]').forEach(c => globalWrappers.push({ id: c.id, el: c as HTMLDivElement }));
     document.querySelectorAll('[sector="true"]').forEach(c => controlWrappers.push({ id: c.id, el: c as HTMLDivElement }));
 
+    // Перед инициализацией включаем фоновые партиклы, чтобы он мог применить к ним состояние (включены / выключены).
     enableParticles();
 
-    initPlugins();
-
-    addOpeningUrlTo('telegram', 'click', 'https://t.me/salarixionion'); 
-    addOpeningUrlTo('discord', 'click', 'https://discord.gg/meSaZdARX'); 
-    addOpeningUrlTo('github', 'click', 'https://github.com/nullclyze/SalarixiOnion'); 
-    addOpeningUrlTo('youtube', 'click', 'https://www.youtube.com/@salarixionion'); 
-
+    // Инициализируем конфигуратор.
     configurator.init();
 
+    // Инициализируем различные менеджеры.
     accountManager.init();
     proxyCollector.init();
     graphic.init();
@@ -1222,18 +1045,41 @@ document.addEventListener('DOMContentLoaded', async () => {
     scriptExecutor.init();
     pinger.init();
 
-    await listenEvents();
-
-    await monitoring.init();
-    await captchaBypass.init();
-    
-    await initFunctions();
-    await translator.init();
+    // Добавляем открывающиеся ссылки к кнопкам (раздел `О проекте`).
+    addOpeningUrlTo('telegram', 'click', 'https://t.me/salarixionion'); 
+    addOpeningUrlTo('discord', 'click', 'https://discord.gg/meSaZdARX'); 
+    addOpeningUrlTo('github', 'click', 'https://github.com/nullclyze/SalarixiOnion'); 
+    addOpeningUrlTo('youtube', 'click', 'https://www.youtube.com/@salarixionion'); 
 
     logger.log('Инициализация прошла успешно', 'extended');
-
-    // await checkUpdate();
   } catch (error) {
-    logger.log(`Ошибка инициализации: ${error}`, 'error');
+    logger.log(`Ошибка инициализации (sync): ${error}`, 'error');
+  }
+});
+
+// Слушатель события DOMContent у document (#2, асинхронный).
+document.addEventListener('DOMContentLoaded', async () => {
+  try {
+    // Изначально инициализируем основные части.
+    await listenEvents();
+
+    await pluginManager.init(); 
+    await monitoring.init();
+    await captchaBypass.init();
+
+    document.getElementById('interface_select_discord-rpc')?.addEventListener('change', async () => await invoke('set_discord_rpc', { state: (document.getElementById('interface_select_discord-rpc') as HTMLSelectElement).value === 'enable' }));
+    await invoke('set_discord_rpc', { state: (document.getElementById('interface_select_discord-rpc') as HTMLSelectElement).value === 'enable' });
+
+    // Контент с web-ресурсов загружаем после основной инициализации, чтобы в случае долгой загрузки не блокировать важные функции.
+    await initUserGuide();
+    await initDownloadCount();
+    
+    await translator.init();
+    await pluginManager.loadDescriptions();
+
+    // Проверка обновлений в последнюю очередь.
+    // await checkUpdate(); 
+  } catch (error) {
+    logger.log(`Ошибка инициализации (async): ${error}`, 'error');
   }
 });
