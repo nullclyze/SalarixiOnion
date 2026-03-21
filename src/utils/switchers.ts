2b2e6293ec8d69306c457b9301803fbb4a94ea6d
nullclyze <nullclyze@gmail.com>
2026-03-21 21:52:46 +0400

    Interface changes

--- src/utils/switchers.ts
+++ src/utils/switchers.ts
@@ -3,13 +3,13 @@ import { controlWrappers, globalWrappers, latestControlWrapper } from '../main';
 /**
  * Функция переключения видимости глобальных обёрток.
  * 
- * При открытии "control-wrapper" функция показывает `latestControlWrapper` (последняя открытая обёртка).
+ * При открытии "control-container" функция показывает `latestControlWrapper` (последняя открытая обёртка).
  * Если `latestControlWrapper` не инициализирован, показывается "control-chat-container".
  */
 const switchGlobalWrapper = (id: string): void => {
   globalWrappers.forEach(w => w && w.id === id ? w.el.style.display = 'flex' : w.el.style.display = 'none');
   controlWrappers.forEach(w => w.el.style.display = 'none');
-  id === 'control-wrapper' ? latestControlWrapper ? latestControlWrapper.style.display = 'flex' : (document.getElementById('control-chat-container') as HTMLElement).style.display = 'flex' : null;
+  id === 'control-container' ? latestControlWrapper ? latestControlWrapper.style.display = 'flex' : (document.getElementById('control-chat-container') as HTMLElement).style.display = 'flex' : null;
 }
 
 /** Функция переключения видимости обёрток управления. */
